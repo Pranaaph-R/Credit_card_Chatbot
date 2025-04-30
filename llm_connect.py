9b4321534c60dee9d5211bd3ff2903caec8ae237
Pranaaph-R <pranaaph.r@gmail.com>
2025-04-30 19:49:58 +0530

    Update llm_connect.py

--- llm_connect.py
+++ llm_connect.py
@@ -8,7 +8,8 @@ import os
 
 load_dotenv()
 
-api_key = os.getenv("GROQ_API_KEY")
+# api_key = os.getenv("GROQ_API_KEY")
+api_key = st.secrets["GROQ_API_KEY"]
 
 #Gorq api configuration
 llm = ChatGroq(
